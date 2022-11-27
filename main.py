15c5f949669065bdeaa770382604ccceb55a9742
Kartik Arora <90651659+KartikAroraOfficial@users.noreply.github.com>
2022-11-27 18:30:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -14,19 +14,24 @@ lcd = LCD()					# uses 0x27 by default
 def safe_exit(signum, frame):
     exit(1)
 
-def display_intensity():
+def display_intensity():    # displays the intensity of light as measured by the measure_intensity functions on both the LCD and the console 
     global message
     while reading:
         print(message)
         lcd.text(message, 1)
+
         sleep(0.25)
 
-def measure_intensity():
+def measure_intensity():    # measures the intensity of light and decides whether it's Dark, Too Dark, Bright, Too Bright or Medium.
     global message
 
     while reading:
+        
+        # reads the data from the I2C system manager bus 
         data = bus.read_i2c_block_data(0x23,0x23)
         sensor_value = ((data[1] + (256*data[0]))/1.2)
+
+        # categorizing the intensity by value 
         if (sensor_value < 30):
             message = "Too Dark"
         elif (sensor_value < 50):
@@ -40,23 +45,35 @@ def measure_intensity():
             
         sleep(0.1)
 
+
+# main body of the program 
 try:
+
+    # exiting safely on receiving terminate/ hang up signal
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
 
+    # 2 threads to run our functions in the background.
     reader = Thread(target=measure_intensity, daemon=True)
     display = Thread(target=display_intensity, daemon=True)
 
+    # starting the threads
     reader.start()
     display.start()
 
-    pause()
+    # pauses the program to wait for the signals
+    pause()     
 
 except KeyboardInterrupt:
     pass
 
 finally:
+    # turning the global variable running false
     reading = False
+    
+    # Ending the background process
     reader.join()
     display.join()
+
+    # clearing the LCD screen
     lcd.clear()
